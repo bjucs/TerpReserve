ec2611921565b70eee016081a29144ae96a26b6f
bjucs <bju@umd.edu>
2024-05-06 20:14:26 -0400

    chore: add Firebase Database SDK + enable internet permissions

--- app/src/main/java/com/groupproject/terpreserve/MainActivity.kt
+++ app/src/main/java/com/groupproject/terpreserve/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.groupproject.terpreserve
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
